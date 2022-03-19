60fd6acf37147dc18f7516c8d310c55d0211998c
Grigory Entin <grigory.entin@gmail.com>
2022-03-19 03:11:29 +0100

    Implemented basic test to allow building the package in Xcode 13.3.

--- Tests/BlessedTests/BlessedTests.swift
+++ Tests/BlessedTests/BlessedTests.swift
@@ -2,10 +2,7 @@ import XCTest
 @testable import Blessed
 
 final class BlessedTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Blessed().text, "Hello, World!")
+    func testAuthorizationInit() throws {
+        try XCTAssertNotNil(Authorization())
     }
 }
