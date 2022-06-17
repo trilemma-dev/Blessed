5e361508c15b3addb14a56a0ee865eeadccb7697
Josh Kaplan <josh@joshkaplan.com>
2022-06-18 00:06:20 +1200

    Bumps the minimum version requirement for Required

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/trilemma-dev/EmbeddedPropertyList.git", from: "2.0.2"),
-        .package(url: "https://github.com/trilemma-dev/Required.git", from: "0.1.0"),
+        .package(url: "https://github.com/trilemma-dev/Required.git", from: "0.1.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
